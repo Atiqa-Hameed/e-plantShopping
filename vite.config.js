83eda4557949799f80208ade595a911f4a73c3b0
Atiqa Hameed <atiqahameed@pop-os.localdomain>
2024-10-26 09:43:58 +0500

    update files

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/shoppingreact",
   plugins: [react()],
-  root: './src', // specify the root folder if needed
-  build: {
-    outDir: '../dist' // ensure the output directory is correct
-  }
 })
